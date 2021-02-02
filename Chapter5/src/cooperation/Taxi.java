9d416967fefe003e2e28ac6e163690df05a21ac4
jaeeunhyun <rosa2070@naver.com>
2021-02-02 18:14:08 +0900

    hohohoho

---
+++ Chapter5/src/cooperation/Taxi.java
@@ -0,0 +1,22 @@
+package cooperation;
+
+public class Taxi {
+
+	int taxiNum;
+	int passengerCount;
+	int money;
+	
+	public Taxi(int taxiNum) {
+		this.taxiNum = taxiNum;
+	}
+	
+	public void take(int money) { //승차
+		this.money += money;
+		passengerCount++;
+		
+	}
+	
+	public void showTaxiInfo() {
+		System.out.println(taxiNum + "번 택시의 승객은 " + passengerCount + "명 이고, 수입은 " + money + "입니다");
+	}
+}
