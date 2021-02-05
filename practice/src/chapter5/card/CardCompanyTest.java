c67e3ff538e22f4807808d6b39e7512b78a59ece
jaeeunhyun <rosa2070@naver.com>
2021-02-05 17:38:53 +0900

    wowowowo

---
+++ practice/src/chapter5/card/CardCompanyTest.java
@@ -0,0 +1,17 @@
+package chapter5.card;
+
+public class CardCompanyTest {
+
+	public static void main(String[] args) {
+
+		CardCompany company = CardCompany.getInstance();
+		
+		Card myCard = company.createCard();
+		Card yourCard = company.createCard();
+		
+		System.out.println(myCard.getCardNumber());
+		System.out.println(yourCard.getCardNumber());
+
+	}
+
+}
