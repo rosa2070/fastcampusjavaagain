bcf8b59bd1b0407e51d13e324b186c7b18c29936
jaeeunhyun <rosa2070@naver.com>
2021-02-05 23:52:54 +0900

    hehehheheeeeee

---
+++ Chapter7/src/inheritance/VIPCustomer.java
@@ -0,0 +1,30 @@
+package inheritance;
+
+public class VIPCustomer extends Customer{
+
+	
+
+	double salesRatio;
+	private int agentID;
+	
+	/*public VIPCustomer() {
+		
+			
+		customerGrade = "VIP";
+		bonusRatio = 0.05;
+		salesRatio = 0.1;
+		
+		System.out.println("VIPCustomer() 생성자 호출");
+	}*/
+
+	public VIPCustomer(int customerID, String customerName) {
+		super(customerID, customerName);
+		
+		customerGrade = "VIP";
+		bonusRatio = 0.05;
+		salesRatio = 0.1;
+		
+		System.out.println("VIPCustomer(int, String) 생성자 호출");
+		
+	}
+}
