21b8a28bb9e166faed686c6da6d2883103c2babe
jaeeunhyun <rosa2070@naver.com>
2021-02-06 00:48:59 +0900

    hmmmm

--- Chapter7/src/polymorphism/AnimalTest.java
+++ Chapter7/src/polymorphism/AnimalTest.java
@@ -1,5 +1,7 @@
 package polymorphism;
 
+import java.util.ArrayList;
+
 class Animal {
 	
 	public void move() {
@@ -34,11 +36,19 @@ public class AnimalTest {
 		Animal tAnimal = new Tiger();
 		Animal eAnimal = new Eagle();
 		
-		AnimalTest test = new AnimalTest();
+		/*AnimalTest test = new AnimalTest();
 		test.moveAnimal(hAnimal);
 		test.moveAnimal(tAnimal);
 		test.moveAnimal(eAnimal);
+		*/
+		ArrayList<Animal> animalList = new ArrayList<Animal>();
+		animalList.add(hAnimal);
+		animalList.add(tAnimal);
+		animalList.add(eAnimal);
 		
+		for(Animal animal : animalList) {
+			animal.move();
+		}
 		
 	}
 	
