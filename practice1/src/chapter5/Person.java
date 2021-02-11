20554c1ff03f5c1a4d4558a943a50a03bff34c60
jaeeunhyun <rosa2070@naver.com>
2021-02-11 23:37:59 +0900

    egeqwg

---
+++ practice1/src/chapter5/Person.java
@@ -0,0 +1,11 @@
+package chapter5;
+
+public class Person {
+	int age;
+	String name;
+	boolean isMarried;
+	int numberOfChildren;
+	
+	
+
+}
---
+++ practice1/src/chapter8/Car.java
@@ -0,0 +1,23 @@
+package chapter8;
+
+public abstract class Car {
+	
+	public abstract void start();
+	public abstract void drive();
+	public abstract void stop();
+	public abstract void turnoff();
+	
+	public void washCar()
+	{
+		System.out.println("세차를 합니다");
+	}
+	
+	final public void run() {
+		start();
+		drive();
+		stop();
+		turnoff();
+		//washCar();
+	}
+
+}
