ab70b4d86fb940043752f66f7298c24e315f412c
jaeeunhyun <rosa2070@naver.com>
2021-02-18 00:03:49 +0900

    sdgwege

--- Chapter13/src/exception/AutoCloseObj.java
+++ Chapter13/src/exception/AutoCloseObj.java
@@ -4,8 +4,7 @@ public class AutoCloseObj implements AutoCloseable{
 
 	@Override
 	public void close() throws Exception {
-		// TODO Auto-generated method stub
-		
+		System.out.println("close()가 호출되었습니다.");
 	}
 	
 	
