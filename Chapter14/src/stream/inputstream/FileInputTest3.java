f560b6f510d7dd9ca9964b52fb8ccc649ff2982f
jaeeunhyun <rosa2070@naver.com>
2021-02-18 23:59:59 +0900

    sdgdsgds

---
+++ Chapter14/src/stream/inputstream/FileInputTest3.java
@@ -0,0 +1,34 @@
+package stream.inputstream;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class FileInputTest3 {
+
+	public static void main(String[] args) {
+
+		FileInputStream fis = null;
+		try {
+			fis = new FileInputStream("input2.txt");
+			
+			int i;
+			while ( (i = fis.read()) != -1) {
+				System.out.print((char)i);
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				fis.close();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		System.out.println("end");
+
+	}
+
+}
