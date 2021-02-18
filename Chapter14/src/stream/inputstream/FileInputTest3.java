fb0d2d11a403080a2f845ab63822b409be7cb988
jaeeunhyun <rosa2070@naver.com>
2021-02-19 00:15:51 +0900

    dsfadfasf

--- Chapter14/src/stream/inputstream/FileInputTest3.java
+++ Chapter14/src/stream/inputstream/FileInputTest3.java
@@ -8,26 +8,26 @@ public class FileInputTest3 {
 
 	public static void main(String[] args) {
 
-		FileInputStream fis = null;
-		try {
-			fis = new FileInputStream("input2.txt");
-			
+		try (FileInputStream fis = new FileInputStream("input2.txt")) {
 			int i;
-			while ( (i = fis.read()) != -1) {
-				System.out.print((char)i);
+			byte[] bs = new byte[10];
+			while ( (i = fis.read(bs)) != -1) {
+				/*for(byte b : bs) {
+					System.out.print((char)b);
+				}*/
+				
+				for(int k=0; k<i; k++) {
+					System.out.print((char)bs[k]);
+				}
+				System.out.println();
 			}
 			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}finally {
-			try {
-				fis.close();
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		}
-		System.out.println("end");
+
+		//System.out.println("end");
 
 	}
 
