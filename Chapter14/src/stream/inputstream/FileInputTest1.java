879bdde9d8f8a06ec624938c66a83933e583a73f
jaeeunhyun <rosa2070@naver.com>
2021-02-18 23:53:27 +0900

    sdgsgdds

---
+++ Chapter14/src/stream/inputstream/FileInputTest1.java
@@ -0,0 +1,36 @@
+package stream.inputstream;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class FileInputTest1 {
+
+	public static void main(String[] args) {
+
+		FileInputStream fis = null;
+		try {
+			fis = new FileInputStream("input.txt");
+			
+			int i = fis.read();
+			System.out.println((char)i);
+			i = fis.read();
+			System.out.println((char)i);
+			i = fis.read();
+			System.out.println((char)i);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				fis.close();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		System.out.println("end");
+
+	}
+
+}
--- Chapter14/src/stream/inputstream/SystemInTest2.java
+++ Chapter14/src/stream/inputstream/SystemInTest2.java
@@ -1,9 +1,24 @@
 package stream.inputstream;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class SystemInTest2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("ÀÔ·Â");
+		
+		try {
+			int i;
+			InputStreamReader isr = new InputStreamReader(System.in);
+			while ( (i = isr.read()) != '³¡') {
+				System.out.print((char)i);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
