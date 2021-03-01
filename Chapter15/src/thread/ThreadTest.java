cee355c4c2d90547f47794f05dc6ee9f90038e9d
jaeeunhyun <rosa2070@naver.com>
2021-03-01 23:33:00 +0900

    dsgasgsdag

---
+++ Chapter15/src/thread/ThreadTest.java
@@ -0,0 +1,48 @@
+package thread;
+
+class MyThread implements Runnable{
+
+
+	public void run() {
+		int i;
+		
+		for( i=0; i<=200; i++) {
+			System.out.print(i + "\t");
+			
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
+
+public class ThreadTest {
+
+	public static void main(String[] args) {
+
+		System.out.println("start");
+		/*MyThread th1 = new MyThread();
+		MyThread th2 = new MyThread();
+		
+		th1.start();
+		th2.start();*/
+		
+		/*MyThread runner1 = new MyThread();
+		Thread th1 = new Thread(runner1);
+		th1.start();
+		
+		MyThread runner2 = new MyThread();
+		Thread th2 = new Thread(runner2);
+		System.out.println("end");
+		th2.start();*/
+		
+		Thread t = Thread.currentThread();
+		System.out.println(t);
+		
+		System.out.println("end");
+
+	}
+
+}
