348e275e43c526fc0d1b43b85b0f4c342643866a
jaeeunhyun <rosa2070@naver.com>
2021-03-01 23:52:04 +0900

    hkjhkl

--- Chapter15/src/thread/TerminateThread.java
+++ Chapter15/src/thread/TerminateThread.java
@@ -1,5 +1,7 @@
 package thread;
 
+import java.io.IOException;
+
 public class TerminateThread extends Thread{
 	
 	private boolean flag = false;
@@ -19,15 +21,40 @@ public class TerminateThread extends Thread{
 				e.printStackTrace();
 			}
 		}
-		System.out.println("end");
+		System.out.println(getName() + " end");
 	}
 	
 	public void setFlag(boolean flag) {
 		this.flag = flag;
 	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+
+		TerminateThread threadA = new TerminateThread("A");
+		TerminateThread threadB = new TerminateThread("B");
+		
+		threadA.start();
+		threadB.start();
+		
+		int in;
+		while(true) {
+			in = System.in.read();
+			if ( in == 'A') {
+				threadA.setFlag(true);
+			}
+			else if (in == 'B'){
+				threadB.setFlag(true);
+			} else if( in == 'M') {
+			
+				threadA.setFlag(true);
+				threadB.setFlag(true);
+				break;
+			}
+			/*else {
+				System.out.println("try again"); \n \r
+			}*/
+		}
+		System.out.println("main end");
 
 	}
 
